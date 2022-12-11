a136094117a9370ab32de116b36cb4e469940aa4
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-12-12 00:33:37 +0300

    Remove Owner Field From Catalog Extension

--- sonar/catalog/models.py
+++ sonar/catalog/models.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import User
 from s2ag.models import *
 
 class CatalogAbstract(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users_%(class)s")
     s2ag_paper_identifiers = models.ManyToManyField(S2AGArticleIdentifier, related_name="s2ag_papers_%(class)s")
 
     class Meta:
         abstract = True
 
 class CatalogBase(CatalogAbstract):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users_%(class)s")
     catalog_name = models.CharField(max_length=255, null=False, blank=False)
 
     class Meta:
