7f0cbf476473ebdae31e3aa5b92f8e2b51ec8e3c
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-12-11 23:32:33 +0300

    Implemented Add and Remove S2AG Paper Identifer to Catalog Base

--- sonar/catalog/views.py
+++ sonar/catalog/views.py
@@ -37,7 +37,56 @@ class CatalogBaseView(APIView):
         return Response({"info": "catalog base creation successful", "catalog_id": catalog_base.id}, status=200)
 
     def put(self, request):
-        pass
+        
+        user = request.user
+        catalog_name = request.data.get('catalog_name', None)
+        edit_type = request.data.get('edit_type', None)
+
+        if catalog_name is None:
+            return Response({'error': 'catalog_name is required'}, status=400)
+
+        if edit_type is None:
+            return Response({'error': 'edit_type is required'}, status=400)
+
+        catalog_base = CatalogBase.objects.filter(owner=user, catalog_name=catalog_name).first()
+
+        if not catalog_base:
+            return Response({'error': 'catalog base not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        if edit_type == "add_s2ag_paper_id":
+
+            s2ag_paper_id = request.data.get('s2ag_paper_id', None)
+
+            if s2ag_paper_id is None:
+                return Response({'error': 's2ag_paper_id is required'}, status=400)
+
+            s2ag_paper_identifier, _ = S2AGArticleIdentifier.objects.get_or_create(s2ag_paperID=s2ag_paper_id)
+            
+            catalog_base.s2ag_paper_identifiers.add(s2ag_paper_identifier)
+
+            catalog_base.save()
+
+            return Response({"info": "s2ag_paper_id: " + s2ag_paper_id + " added to catalog base: " + catalog_name}, status=200)
+
+        if edit_type == "remove_s2ag_paper_id":
+
+            s2ag_paper_id = request.data.get('s2ag_paper_id', None)
+
+            if s2ag_paper_id is None:
+                return Response({'error': 's2ag_paper_id is required'}, status=400)
+
+            catalog_base_s2ag_paper_identifiers = catalog_base.s2ag_paper_identifiers.all()
+
+            s2ag_paper_identifier = S2AGArticleIdentifier.objects.filter(s2ag_paperID=s2ag_paper_id).first()
+
+            if s2ag_paper_identifier not in catalog_base_s2ag_paper_identifiers:
+                return Response({'error': 's2ag_paper_id: ' + s2ag_paper_id + ' not in catalog base'}, status=400)
+            
+            catalog_base.s2ag_paper_identifiers.remove(s2ag_paper_identifier)
+
+            catalog_base.save()
+
+            return Response({"info": "s2ag_paper_id: " + s2ag_paper_id + " removed from catalog base"}, status=200)
 
     def delete(self, request):
         pass
