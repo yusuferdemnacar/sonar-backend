5be7b5a72eb7a50ad9538a5be36c95e8bfca286e
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-12-12 00:01:19 +0300

    Added S2AGArticleIdentifier to Admin Panel

--- sonar/s2ag/admin.py
+++ sonar/s2ag/admin.py
@@ -4,4 +4,8 @@ from .models import *
 class S2AGSearchDisplayArticleAdmin(admin.ModelAdmin):
     list_display = ("title",)
 
+class S2AGArticleIdentifierAdmin(admin.ModelAdmin):
+    list_display = ("s2ag_paperID",)
+
 admin.site.register(S2AGSearchDisplayArticle, S2AGSearchDisplayArticleAdmin)
+admin.site.register(S2AGArticleIdentifier, S2AGArticleIdentifierAdmin)
