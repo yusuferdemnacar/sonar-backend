22fa56d37e7d6b01df121baa2dfb429d3ec49bb4
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-12-12 21:14:41 +0300

    Removed Unnecessary Imports

--- sonar/catalog/views.py
+++ sonar/catalog/views.py
@@ -1,12 +1,10 @@
 from rest_framework.views import APIView
-from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from .serializers import *
 from .models import *
 import requests
-from termcolor import colored
 
 class CatalogBaseView(APIView):
 
