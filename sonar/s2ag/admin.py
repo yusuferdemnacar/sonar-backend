b11d30acf15a5a9c07ab50f5bde9baeb4aa76d20
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-12-19 21:22:39 +0300

    Added __str__ Methods to S2AG Models to Better Utilize Admin Panel

--- sonar/s2ag/admin.py
+++ sonar/s2ag/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from .models import *
 
-class S2AGSearchDisplayArticleAdmin(admin.ModelAdmin):
-    list_display = ("title",)
-
-class S2AGArticleIdentifierAdmin(admin.ModelAdmin):
-    list_display = ("s2ag_paperID",)
-
-admin.site.register(S2AGSearchDisplayArticle, S2AGSearchDisplayArticleAdmin)
-admin.site.register(S2AGArticleIdentifier, S2AGArticleIdentifierAdmin)
+admin.site.register(S2AGSearchDisplayArticle)
+admin.site.register(S2AGArticleIdentifier)
