c0b64b4944e768b210f2710ab50337466b46d595
yusuferdemnacar <yusuferdemnacar@gmail.com>
2023-01-15 18:13:05 +0300

    Added Tests for Catalog and Search
    Added basic tests for Catalog and Search to test the core
    functionalities of the apps. More detailed tests will be added.

--- sonar/search/tests.py
+++ sonar/search/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.test import Client
+
+#use double quotes insted of single quotes
+class SearchTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_search_success(self):
+        response = self.client.get("/search/s2ag_search/", {"search_query": "click chemistry", "offset": 1})
+        self.assertEqual(response.status_code, 200)
+        self.assertCountEqual(response.json().keys(), ["search_results", "total_page_count", "search_query", "offset"])
+        self.assertEqual(response.json()["search_query"], "click chemistry")
 
 # Create your tests here.
