31a5e85ddde7c8f4df9c14abfc76d7ca729e63d1
yusuferdemnacar <yusuferdemnacar@gmail.com>
2023-02-05 00:25:21 +0300

    Added multigraph support to neo4j_analysis_client.py

--- sonar/analyze/neo4j_analysis_client.py
+++ sonar/analyze/neo4j_analysis_client.py
@@ -10,17 +10,17 @@ class Neo4jAnalysisClient(Neo4jClient):
         
     def _calculate_betweenness(tx, username, node_type, edge_type):
 
-        graph_exists_query = """CALL gds.graph.exists('graph') YIELD exists"""
+        graph_exists_query = """CALL gds.graph.exists('{username}') YIELD exists""".format(username=username)
 
         if tx.run(graph_exists_query).data()[0]['exists']:
 
-            remove_graph_query = """CALL gds.graph.drop('graph')"""
+            remove_graph_query = """CALL gds.graph.drop('{username}')""".format(username=username)
             tx.run(remove_graph_query)
 
-        named_graph_query = """CALL gds.graph.project('graph', '{node_type}', '{edge_type}')""".format(
-            username=username, node_type=node_type, edge_type=edge_type)
+        named_graph_query = """CALL gds.graph.project.cypher('{username}', 'MATCH (n:{node_type}) WHERE n.owner = "{username}" RETURN id(n) AS id', 'MATCH (n:{node_type}) WHERE n.owner = "{username}" MATCH (n)-[r:{edge_type}]->(m) RETURN id(n) AS source, id(m) AS target, type(r) AS type')""".format(username=username, node_type=node_type, edge_type=edge_type)
+        print(named_graph_query)
 
-        betweenness_query = """CALL gds.betweenness.stream('graph') YIELD nodeId, score RETURN gds.util.asNode(nodeId) AS Article, score AS betweenness_centrality_score ORDER BY score DESC"""
+        betweenness_query = """CALL gds.betweenness.stream('{username}') YIELD nodeId, score RETURN gds.util.asNode(nodeId) AS Article, score AS betweenness_centrality_score ORDER BY score DESC""".format(username=username)
 
         tx.run(named_graph_query)
         return tx.run(betweenness_query).data()
