665153447c4c19048025c2f396889025f8b36f3e
yusuferdemnacar <yusuferdemnacar@gmail.com>
2023-05-21 19:26:15 +0300

    Handled error responses returned to the  time series difference endpoints from the time series endpoints.

--- sonar/analyze/views.py
+++ sonar/analyze/views.py
@@ -286,7 +286,14 @@ class DiffTimeSeriesCentralityView(TimeSeriesCentralityView):
     
     def get_differetial_scores(self, request, score_function):
 
-        scores = self.get_scores(request, score_function).data
+        #TODO: Take response creation to the outermost function
+
+        scores = self.get_scores(request, score_function)
+
+        if scores.status_code != status.HTTP_200_OK:
+            return scores
+        
+        scores = scores.data
 
         node_type = request.query_params.get('node_type', None)
         edge_type = request.query_params.get('edge_type', None)
